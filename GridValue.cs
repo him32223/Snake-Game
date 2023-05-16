89189da120ac1243ba6ad22322629211a264803c
Bryan William <bryandenetto26@outlook.com>
2023-05-16 09:48:31 +0800

    Created a new class for the grid value

---
+++ GridValue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_Game
+{
+    public enum GridValue
+    {
+        //We have name the Grid//
+        Empty,
+        Snake,
+        Food,
+        Outside,
+
+    }
+}
